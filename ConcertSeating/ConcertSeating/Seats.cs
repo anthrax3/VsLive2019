43fdbdd0cecccb4e5ec5131a93fe719ba0754888
Jon Galloway <jongalloway@gmail.com>
2019-10-11 11:23:35 -0700

    Added ConcertSeating demo

---
+++ ConcertSeating/ConcertSeating/Seats.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace ConcertSeating
+{
+    public class Seat : Hub
+    {
+        public Task Select(int row, int col)
+        {
+           return Clients.All.SendAsync("Select", row, col);
+        }
+    }
+}
